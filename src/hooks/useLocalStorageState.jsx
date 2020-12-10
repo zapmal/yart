dca62da51b3741a2a64ec2f413fe963c1165b397
Zondazx <mzxgd16@gmail.com>
2020-12-10 11:56:18 -0400

    feat: added custom hook for local storage

---
+++ src/hooks/useLocalStorageState.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+const useLocalStorageState = key => {
+  const [value, setValue] = useState(JSON.parse(localStorage.getItem(key)) || []);
+  
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorageState;
