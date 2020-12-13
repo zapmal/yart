f5682425c96e6939a64d516c60d08a4c2fd4c0ab
Zondazx <mzxgd16@gmail.com>
2020-12-13 17:03:47 -0400

    fix: remove unnecessary data

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const DATA = [
-  { id: 'todo-0', name: 'Eat', completed: true },
-  { id: 'todo-1', name: 'Rest', completed: false },
-  { id: 'todo-2', name: 'Sleep', completed: false },
-];
-
 ReactDOM.render(<App />, document.getElementById('root'));
